bebc5a245f194ecfd02a4e90deaf4443bfcc1ea6
Ayesha <ayesha.cse14@gmail.com>
2020-01-12 12:08:03 +0600

    first commit

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,13 @@
+ 
+@extends('layouts.master')
+
+@section('content')
+  <div class='container margin-top-20' id="contactPage" >
+    <h2>Contact With us: </h2>
+    <br>
+    <b>Email: </b>
+     <p>ayesha.cse14@gmail.com</p>
+     <b>Phone: </b>
+     <p>+880171234567</p>
+  </div>
+@endsection
